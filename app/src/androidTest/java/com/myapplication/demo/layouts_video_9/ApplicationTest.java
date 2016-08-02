6335d509296977f35ad3015fb4f1afc6b225c03e
Anderson Eduardo Jimenez Prieto <andersonmurdog@gmail.com>
2016-08-02 12:37:14 -0500

    Initial commit Esta realizado la anidacion de layouts y la estructura de pesos

---
+++ app/src/androidTest/java/com/myapplication/demo/layouts_video_9/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.myapplication.demo.layouts_video_9;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
