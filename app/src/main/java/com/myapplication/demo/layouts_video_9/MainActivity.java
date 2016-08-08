300b3ad44aaa54a70f571a427fa100d9522ce973
Anderson Eduardo Jimenez Prieto <andersonmurdog@gmail.com>
2016-08-08 13:43:09 -0500

    Boton 1 con evento click usando metodo inline https://github.com/anderson-0812/Layouts_video_9/issues/1

--- app/src/main/java/com/myapplication/demo/layouts_video_9/MainActivity.java
+++ app/src/main/java/com/myapplication/demo/layouts_video_9/MainActivity.java
@@ -1,13 +1,38 @@
 package com.myapplication.demo.layouts_video_9;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton1;
+    Button boton2;
+    Button boton3;
+    Button boton4;
+    Button boton5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton1 = (Button)findViewById(R.id.btn_1);
+
+        //metodo click inline
+        boton1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Context context = getApplicationContext();
+                CharSequence text = "Boton 1 pulsado!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
 }
